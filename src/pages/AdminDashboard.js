d40d0d3a44b53090b1b349b8760e01af9f999a78
iqra  Khaliq <iqra.khaliq459@gmail.com>
2021-06-05 08:22:26 +0500

    Admin dashboard Change

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -1,32 +1,74 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default class AdminDashboard extends React.Component
 {
     render()
     {
         return(
-            <View>
-                <Button 
+            <View style={styles.container}>
+                {/* <Button 
                     color={"#fa8072"} 
                     title="Donated Items"
-                    size={25}  
+                    size={45}  
                     onPress={() => this.props.navigation.navigate('DonorDetails')}
                     />
                 <Button 
                     color={"#fa8072"} 
                     title="Users Feedback"
-                    size={25}  
+                    size={45}  
                     onPress={() => this.props.navigation.navigate('FeedbackList')}
-                    />
-                <Button 
-                    color={"#fa8072"} 
-                    title="Users List"
-                    size={25}  
-                    onPress={() => this.props.navigation.navigate('Users')}
-                    />
+                    /> */}
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => this.props.navigation.navigate('DonorDetails')}
+                        >
+                        <Text style={styles.txtStyl}>Donated Items</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => this.props.navigation.navigate('FeedbackList')}
+                        >
+                        <Text style={styles.txtStyl}>Users Feedback</Text>
+                    </TouchableOpacity>
+                
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => this.props.navigation.navigate('Users')}
+                        >
+                        <Text style={styles.txtStyl}>Users List</Text>
+                    </TouchableOpacity>
                   
             </View>
         )
     }
 }
+
+const styles=StyleSheet.create({
+    container:
+    {
+        paddingTop: 180,
+        margin: 10,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        flexDirection: 'column',
+        flex: 1,
+    },
+    button: 
+    {
+        alignItems: 'center',
+        backgroundColor: '#fa8072',
+        paddingLeft: 20,
+        paddingRight: 20,
+        marginTop: 35,
+        padding: 5,
+        width: 350,
+    },
+    txtStyl:
+    {
+        color:'white',
+        fontWeight:'500',
+        fontSize: 40,
+    },
+});
